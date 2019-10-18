7cd0ee5cdb09c70c1456194ea11511ac1bda0f1b
noliaki <noliaki@gmail.com>
2019-10-18 14:23:33 +0900

    [wip]

--- src/js/glsl/vertex-shader.glsl
+++ src/js/glsl/vertex-shader.glsl
@@ -1,12 +1,19 @@
+attribute vec3 place;
+attribute vec3 staggerTime;
 uniform float uTime;
 varying vec3 vUv;
 
-void main() {
-  // vUv = hsv(abs(sin(uTime / 300.0)) + (normalize(position.x) + 1.0) / 2.0, 0.6, 0.7);
-  float colorAvg = (position.x - position.y + position.z) / 3.0;
-  float h = (sin((uTime) / 100.0 + colorAvg) + 1.0) / 2.0;
+const float PI = 3.141592653589793238462643383279502884197169399375105820;
+const float PI2 = 2.0 * PI;
+
+void main(){
+ // vUv = hsv(abs(sin(uTime / 300.0)) + (normalize(position.x) + 1.0) / 2.0, 0.6, 0.7);
+  float colorAvg = (position.x + position.y + position.z) / 3.0;
+  float h = (sin(uTime / 50.0 + staggerTime.z) + 1.0) / 2.0;
   vUv = hsv(h, 0.6, 0.7);
-  vec4 transformed = vec4(position, 1.0);
+  vec4 quat = quatFromAxisAngle(vec3(1.0), radians(uTime + staggerTime.x));
+  vec4 transformed = vec4(rotateVector(quat, (position - place)) + position, 1.0);
+  // vec4(rotate3d(position, radians(uTime / 10.0), vec3(1.0, 0.7, 0.0)), 1.0);
   // transformed.x += (h * 50.0);
   // transformed.z += cos(uTime / 10.0) * 50.0;
   gl_Position = projectionMatrix * modelViewMatrix * transformed;
--- src/js/particle-geometry.ts
+++ src/js/particle-geometry.ts
@@ -9,7 +9,7 @@ import {
 
 import { getRandomPointOnSphere } from './helper'
 
-const count: number = 10000
+const count: number = 100000
 const dimension: number = 3
 const itemVertices: number = 3
 const faceVertices: number = itemVertices * dimension
@@ -18,21 +18,33 @@ const totalVertices: number = count * faceVertices
 // const planeGeometry: PlaneBufferGeometry = new PlaneBufferGeometry(10, 10, 1, 1)
 
 const geometry: BufferGeometry = new BufferGeometry()
-const vertices: Float32Array = new Float32Array(totalVertices)
+const position: Float32Array = new Float32Array(totalVertices)
+const place: Float32Array = new Float32Array(totalVertices)
+const staggerTime: Float32Array = new Float32Array(totalVertices)
 const indeces: Uint32Array = new Uint32Array(count * itemVertices)
 
 for (let i: number = 0; i < count; i++) {
   const center: Vector3 = getRandomPointOnSphere(
-    ThreeMath.randFloat(200, 10000)
+    ThreeMath.randFloat(100, 100000)
   )
 
   for (let j: number = 0; j < itemVertices; j++) {
     const vertex: Vector3 = getRandomPointOnSphere(ThreeMath.randFloat(10, 50))
     const index: number = i * faceVertices + j * dimension
 
-    vertices[index + 0] = center.x + vertex.x
-    vertices[index + 1] = center.y + vertex.y
-    vertices[index + 2] = center.z + vertex.z
+    position[index + 0] = center.x + vertex.x
+    position[index + 1] = center.y + vertex.y
+    position[index + 2] = center.z + vertex.z
+
+    place[index + 0] = center.x
+    place[index + 1] = center.y
+    place[index + 2] = center.z
+
+    // const timeRatio: number = ThreeMath.randFloat(20, 200)
+
+    staggerTime[index + 0] = ThreeMath.randFloat(20, 200)
+    staggerTime[index + 1] = Math.random()
+    staggerTime[index + 2] = ThreeMath.randFloat(20, 200)
   }
 }
 
@@ -42,7 +54,12 @@ indeces.forEach((val: number, index: number, arr: Uint32Array): void => {
 
 console.log(geometry)
 
-geometry.addAttribute('position', new BufferAttribute(vertices, dimension))
+geometry.addAttribute('position', new BufferAttribute(position, dimension))
+geometry.addAttribute('place', new BufferAttribute(place, dimension))
+geometry.addAttribute(
+  'staggerTime',
+  new BufferAttribute(staggerTime, dimension)
+)
 geometry.setIndex(new BufferAttribute(indeces, 1))
 
 export default geometry
